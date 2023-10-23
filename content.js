13e640c386427255d2b4de6776212418f287e0b5
madortill <mador.till@gmail.com>
2023-10-23 10:42:35 +0300

    hadar content

--- content.js
+++ content.js
@@ -48,7 +48,7 @@ const DATA = [
                     type: `crossroadQuestion`,
                     question: `איך מגיעים הביתה?`,
                     // recomended: no more than 16 answers, 8 if the are longer than one word
-                    answers: {"תחבורה ציבורית (כולל שאטלים)": "תחבצ", "טרמפים" : "טרמפים", "רכב פרטי" : "רכב"},// the legth will determine how many answers will apear
+                    answers: { "תחבורה ציבורית (כולל שאטלים)": "תחבצ", "טרמפים": "טרמפים", "רכב פרטי": "רכב" },// the legth will determine how many answers will apear
                 },
             ],
             'תחבצ': [
@@ -58,12 +58,12 @@ const DATA = [
                     יש להישאר מדוגם במשך כל הנסיעה \n
                     יש לנוע רק עם אוזניה אחת. \n
                     יש לגלות ערנות ולחפש סימנים מחשידים שבהם ניזכר בהמשך.`
-                }, 
+                },
                 {
                     type: `crossroadQuestion`,
                     question: `זוכר/ת שאסור לעלות על טרמפים?`,
                     // recomended: no more than 16 answers, 8 if the are longer than one word
-                    answers: {"כן": "איסור טרמפים", "לא" : "איסור טרמפים"},
+                    answers: { "כן": "איסור טרמפים", "לא": "איסור טרמפים" },
                 },
             ],
             "טרמפים": [
@@ -71,9 +71,9 @@ const DATA = [
                     type: `crossroadQuestion`,
                     question: `זוכר/ת שאסור לעלות על טרמפים?`,
                     // recomended: no more than 16 answers, 8 if the are longer than one word
-                    answers: {"כן": "איסור טרמפים טרמפ", "לא" : "איסור טרמפים טרמפ"}
+                    answers: { "כן": "איסור טרמפים טרמפ", "לא": "איסור טרמפים טרמפ" }
                 }
-            ], 
+            ],
             "איסור טרמפים": [
                 {
                     type: `manyChoices`,
@@ -83,7 +83,7 @@ const DATA = [
                     correctAns: [`ans1`, `ans2`, `ans3`] // the legth will determine how many answers are reqwiered
                 },
                 {
-                    type: "info", 
+                    type: "info",
                     content: `לא לשכוח את הדגשים לנסיעה ברכב פרטי: \n
                     1. 7 שעות שינה לפני נהיגה. \n
                     2. אין לנהוג תחת השפעת אלכוהול. \n
@@ -94,7 +94,7 @@ const DATA = [
                 {
                     type: "crossroadQuestion",
                     question: "האם אתה יודע מהם הסימנים המחשידים?",
-                    answers: {"כן" : "מכיר סימנים", "לא" : "לא מכיר סימנים"}
+                    answers: { "כן": "מכיר סימנים", "לא": "לא מכיר סימנים" }
                 }
             ],
             "איסור טרמפים טרמפ": [
@@ -114,7 +114,7 @@ const DATA = [
                     יש לגלות ערנות ולחפש סימנים מחשידים שבהם ניזכר בהמשך.`
                 },
                 {
-                    type: "info", 
+                    type: "info",
                     content: `לא לשכוח גם את הדגשים לנסיעה ברכב פרטי: \n
                     1. 7 שעות שינה לפני נהיגה. \n
                     2. אין לנהוג תחת השפעת אלכוהול. \n
@@ -125,12 +125,12 @@ const DATA = [
                 {
                     type: "crossroadQuestion",
                     question: "האם אתה יודע מהם הסימנים המחשידים?",
-                    answers: {"כן" : "מכיר סימנים", "לא" : "לא מכיר סימנים"}
+                    answers: { "כן": "מכיר סימנים", "לא": "לא מכיר סימנים" }
                 }
-            ], 
+            ],
             "רכב": [
                 {
-                    type: "info", 
+                    type: "info",
                     content: `1. 7 שעות שינה לפני נהיגה. \n
                     2. אין לנהוג תחת השפעת אלכוהול. \n
                     3. יש לנהוג בהתאם לתנאי מזג האויר. \n 
@@ -155,9 +155,9 @@ const DATA = [
                 {
                     type: "crossroadQuestion",
                     question: "האם אתה יודע מהם הסימנים המחשידים?",
-                    answers: {"כן" : "מכיר סימנים", "לא" : "לא מכיר סימנים"}
+                    answers: { "כן": "מכיר סימנים", "לא": "לא מכיר סימנים" }
                 }
-            ], 
+            ],
             "מכיר סימנים": [
                 {
                     type: "info",
@@ -169,9 +169,9 @@ const DATA = [
                     type: `crossroadQuestion`,
                     question: `תבחר/י בתמונה בה מופיע אדם חשוד`,
                     // recomended: no more than 16 answers, 8 if the are longer than one word
-                    answers: {"../assets/images/questionsPics/exemple8.jpg": "חשוד", "../assets/images/questionsPics/exemple8.jpg": "לא חשוד"},// the legth will determine how many answers will apear
+                    answers: { "../assets/images/questionsPics/exemple8.jpg": "חשוד", "../assets/images/questionsPics/exemple8.jpg": "לא חשוד" },// the legth will determine how many answers will apear
                 },
-            ], 
+            ],
             "לא מכיר סימנים": [
                 {
                     type: "info",
@@ -182,7 +182,7 @@ const DATA = [
                     type: `crossroadQuestion`,
                     question: `תבחר/י בתמונה בה מופיע אדם חשוד`,
                     // recomended: no more than 16 answers, 8 if the are longer than one word
-                    answers: {"../assets/images/questionsPics/exemple8.jpg": "חשוד", "../assets/images/questionsPics/exemple8.jpg": "לא חשוד"},// the legth will determine how many answers will apear
+                    answers: { "../assets/images/questionsPics/exemple8.jpg": "חשוד", "../assets/images/questionsPics/exemple8.jpg": "לא חשוד" },// the legth will determine how many answers will apear
                 },
             ],
             "חשוד": [
@@ -204,77 +204,131 @@ const DATA = [
         }
     },
     {
-            pic: "../assets/images/questionsPics/example1.jpg",
-            title: "אבטחת נשק",
-            curretntQuestion: [0, "הקדמה"],
-            status: "לא הותחל",
-            startSubSubject: "הקדמה",
-            content: {
-                "הקדמה": [
-                    {
-                        type: "crossroadQuestion",
-                        question: "יוצאים עם נשק הביתה?",
-                        answers:  {"כן": "יציאה עם נשק", "לא": "נכסל"} //key = answer, value = subSubject to go to
-                    }
-                ],
-                "יציאה עם נשק": [
-                    {
-                        type: `manyChoices`,
-                        question: `האם עליי לקבל אישור ממפקד היחידה?`,
-                        // recomended: no more than 16 answers, 8 if the are longer than one word
-                        answers: ["נכון", "לא נכון"],// the legth will determine how many answers will apear
-                        correctAns: [`ans1`], // the legth will determine how many answers are reqwiered
-                        explain: `בשביל לצאת עם נשק הביתה נדרש אישור בדרגת אל״ם ומעלה.`
-                    },
-                    {
-                        type: `manyChoices`,
-                        question: `האם מותר להניח את הנשק מתחת לכיסא במהלך הנסיעה?`,
-                        // recomended: no more than 16 answers, 8 if the are longer than one word
-                        answers: ["נכון", "לא נכון"],// the legth will determine how many answers will apear
-                        correctAns: [`ans2`], // the legth will determine how many answers are reqwiered
-                        explain: `יש לשאת את הנשק עלי במהלך כל הנסיעה עם הקנה כלפי מטה.`
-                    },
-                    {
-                        type: `manyChoices`,
-                        question: `האם מותר להניח את הנשק מתחת לכיסא במהלך הנסיעה?`,
-                        // recomended: no more than 16 answers, 8 if the are longer than one word
-                        answers: ["נכון", "לא נכון"],// the legth will determine how many answers will apear
-                        correctAns: [`ans2`], // the legth will determine how many answers are reqwiered
-                        explain: `הנשק שאיתו יצאת הינו הנשק האישי שלך ואתה היחיד שאמון על בטחונו.`
-                    },
-                    {
-                        type: `manyChoices`,
-                        question: `אבטחת הנשק בבית מאחורי שני מנעולים והמחסנית בנפרד?`,
-                        // recomended: no more than 16 answers, 8 if the are longer than one word
-                        answers: ["נכון", "לא נכון"],// the legth will determine how many answers will apear
-                        correctAns: [`ans1`], // the legth will determine how many answers are reqwiered
-                        explain: `אבטחת הנשק בבית מאחורי שני מנעולים והמחסנית בנפרד הינה פקודה.`
-                    },
-                    {
-                        type: `manyChoices`,
-                        question: `האם חובה להפריד מכלול מהנשק ולהחביאו במקום אחר?`,
-                        // recomended: no more than 16 answers, 8 if the are longer than one word
-                        answers: ["נכון", "לא נכון"],// the legth will determine how many answers will apear
-                        correctAns: [`ans1`], // the legth will determine how many answers are reqwiered
-                        explain: `הפרדת מכלול מהנשק והחבאתו במקום אחר הינה פקודה.`
-                    },
-                    {
-                        type: `manyChoices`,
-                        question: `במידה ולא ניתן לאבטח את הנשק לפי הכללים אפקיד בתחנת משטרה צבאית?`,
-                        // recomended: no more than 16 answers, 8 if the are longer than one word
-                        answers: ["נכון", "לא נכון"],// the legth will determine how many answers will apear
-                        correctAns: [`ans1`], // the legth will determine how many answers are reqwiered
-                        explain: `אם נבצר ממך לאבטח את הנשק ע״פ הפקודות עליך להפקידו במקום צבאי המורשה לכך.`
-                    },
-                    {
-                        type: `manyChoices`,
-                        question: `האם מותר להשתמש בנשק לטובת עבודה אזרחית בסופש?`,
-                        // recomended: no more than 16 answers, 8 if the are longer than one word
-                        answers: ["נכון", "לא נכון"],// the legth will determine how many answers will apear
-                        correctAns: [`ans2`], // the legth will determine how many answers are reqwiered
-                        explain: `הנשק הינו רכוש צהל, ופעולה זו מהווה עבירה בציר הצבאי והן האזרחי.`
-                    },
-                ]
-            }
-        },
+        pic: "../assets/images/questionsPics/example1.jpg",
+        title: "אבטחת נשק",
+        curretntQuestion: [0, "הקדמה"],
+        status: "לא הותחל",
+        startSubSubject: "הקדמה",
+        content: {
+            "הקדמה": [
+                {
+                    type: "crossroadQuestion",
+                    question: "יוצאים עם נשק הביתה?",
+                    answers: { "כן": "יציאה עם נשק", "לא": "נכסל" } //key = answer, value = subSubject to go to
+                }
+            ],
+            "יציאה עם נשק": [
+                {
+                    type: `manyChoices`,
+                    question: `האם עליי לקבל אישור ממפקד היחידה?`,
+                    // recomended: no more than 16 answers, 8 if the are longer than one word
+                    answers: ["נכון", "לא נכון"],// the legth will determine how many answers will apear
+                    correctAns: [`ans1`], // the legth will determine how many answers are reqwiered
+                    explain: `בשביל לצאת עם נשק הביתה נדרש אישור בדרגת אל״ם ומעלה.`
+                },
+                {
+                    type: `manyChoices`,
+                    question: `האם מותר להניח את הנשק מתחת לכיסא במהלך הנסיעה?`,
+                    // recomended: no more than 16 answers, 8 if the are longer than one word
+                    answers: ["נכון", "לא נכון"],// the legth will determine how many answers will apear
+                    correctAns: [`ans2`], // the legth will determine how many answers are reqwiered
+                    explain: `יש לשאת את הנשק עלי במהלך כל הנסיעה עם הקנה כלפי מטה.`
+                },
+                {
+                    type: `manyChoices`,
+                    question: `האם מותר להניח את הנשק מתחת לכיסא במהלך הנסיעה?`,
+                    // recomended: no more than 16 answers, 8 if the are longer than one word
+                    answers: ["נכון", "לא נכון"],// the legth will determine how many answers will apear
+                    correctAns: [`ans2`], // the legth will determine how many answers are reqwiered
+                    explain: `הנשק שאיתו יצאת הינו הנשק האישי שלך ואתה היחיד שאמון על בטחונו.`
+                },
+                {
+                    type: `manyChoices`,
+                    question: `אבטחת הנשק בבית מאחורי שני מנעולים והמחסנית בנפרד?`,
+                    // recomended: no more than 16 answers, 8 if the are longer than one word
+                    answers: ["נכון", "לא נכון"],// the legth will determine how many answers will apear
+                    correctAns: [`ans1`], // the legth will determine how many answers are reqwiered
+                    explain: `אבטחת הנשק בבית מאחורי שני מנעולים והמחסנית בנפרד הינה פקודה.`
+                },
+                {
+                    type: `manyChoices`,
+                    question: `האם חובה להפריד מכלול מהנשק ולהחביאו במקום אחר?`,
+                    // recomended: no more than 16 answers, 8 if the are longer than one word
+                    answers: ["נכון", "לא נכון"],// the legth will determine how many answers will apear
+                    correctAns: [`ans1`], // the legth will determine how many answers are reqwiered
+                    explain: `הפרדת מכלול מהנשק והחבאתו במקום אחר הינה פקודה.`
+                },
+                {
+                    type: `manyChoices`,
+                    question: `במידה ולא ניתן לאבטח את הנשק לפי הכללים אפקיד בתחנת משטרה צבאית?`,
+                    // recomended: no more than 16 answers, 8 if the are longer than one word
+                    answers: ["נכון", "לא נכון"],// the legth will determine how many answers will apear
+                    correctAns: [`ans1`], // the legth will determine how many answers are reqwiered
+                    explain: `אם נבצר ממך לאבטח את הנשק ע״פ הפקודות עליך להפקידו במקום צבאי המורשה לכך.`
+                },
+                {
+                    type: `manyChoices`,
+                    question: `האם מותר להשתמש בנשק לטובת עבודה אזרחית בסופש?`,
+                    // recomended: no more than 16 answers, 8 if the are longer than one word
+                    answers: ["נכון", "לא נכון"],// the legth will determine how many answers will apear
+                    correctAns: [`ans2`], // the legth will determine how many answers are reqwiered
+                    explain: `הנשק הינו רכוש צהל, ופעולה זו מהווה עבירה בציר הצבאי והן האזרחי.`
+                },
+            ]
+        }
+    },
+    {
+        pic: "../assets/images/logo.svg",
+        title: 'נכס״ל',
+        curretntQuestion: [0, "הקדמה"],
+        status: "לא הותחל",
+        startSubSubject: "הקדמה",
+        content: {
+            'נכסל': [
+                {
+                    type: "crossroadQuestion",
+                    question: "יודעים מהי הנחיית הזמינות בסופ״ש?",
+                    answers: { "כן": "הסבר", "לא": "הסבר" } //key = answer, value = subSubject to go to
+                },
+            ],
+            "הסבר": [
+                {
+                    type: "info",
+                    content: "זמינות להפעלת נכס לאומי ע״י המפקדים"
+                },
+                {
+                    type: "info",
+                    content: "טלפון דלוק במהלך השבת למקרה של הקפצה"
+                },
+                {
+                    type: "info",
+                    content: "הכרת מספרי המפקדים והחמ״ל"
+                },
+                {
+                    type: "crossroadQuestion",
+                    question: "במידה ויש הקפצה במהלך הסופ״ש יש לך איך להגיע לבסיס?",
+                    answers: { "כן": "סיום נכסל", "לא": "סיום נכסל" } //key = answer, value = subSubject to go to
+                },
+            ],
+            "סיום נכסל": [
+                {
+                    type: "info",
+                    content: "יש לעדכן את המפקדים וזכור: לא עולים על טרמפים!"
+                },
+                {
+                    type: "crossroadQuestion",
+                    question: "פעילים ברשתות חברתיות?",
+                    answers: { "פייסבוק": "בטחון מידע", "אינסטגרם": "בטחון מידע", "וואצאפ": "בטחון מידע", "גם וגם": "בטחון מידע", "אחר": "בטחון מידע" } //key = answer, value = subSubject to go to
+                },
+            ],
+            "בטחון מידע": [
+                {
+                    type: "info",
+                    content: " אין להעלות לרשתות החברתיות תוכן צבאי, לשוחח על נושאים צבאיים, לנקוב מיקומים ולמנות סד״כ ברשתות או במקומות אזרחיים."
+                },
+            ]
+
+
+        }
+    },
 ]
